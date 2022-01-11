a899e4739ee58bb21cc4be3fbcda0cb358a9f8fe
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-01-11 14:07:44 +0800

    Add install ideas

---
+++ install/brew.sh
@@ -0,0 +1,22 @@
+brew install micro
+brew install nvm
+mkdir ~/.nvm
+
+brew install lsd
+brew install thefuck
+brew install tmux
+
+brew install --cask visual-studio-code
+brew install --cask microsoft-edge
+brew install --cask slack
+brew install --cask adobe-acrobat-reader
+brew install --cask discord
+brew install --cask figma
+brew install --cask iterm2
+brew install --cask google-drive
+# Postman alternative?
+# Mailspring alternative
+brew install --cask vlc
+brew install --cask zoom
+brew install --cask autodesk-fusion360
+brew install --cask drawio

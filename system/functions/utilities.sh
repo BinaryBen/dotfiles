8df57ba448b60c093af80c995c3626ac9c833c84
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-09-09 00:20:22 +0800

    🚧 May or may not stay

---
+++ system/functions/utilities.sh
@@ -0,0 +1,28 @@
+function simulate_exit_code() {
+    return "$1"
+}
+
+function get_display_length () {
+    formatted_string=$(echo ${1})
+    echo "${#formatted_string}"
+}
+
+# Copy the pwd to the clipboard. Optionally accept an argument for a file to append.
+function pwd2cb () {
+
+}
+
+# Download a file/URL/etc using appropriate tools
+function dl () {
+    # Planned support for: Curl, Wget, HTTPie, Youtube-dl, aria2c, Motrix, axel
+}
+
+# Calculator
+function = () {
+    echo "I'm not working. I would like to. Code me please!"
+}
+
+# Search using SearX
+function sx () {
+
+}

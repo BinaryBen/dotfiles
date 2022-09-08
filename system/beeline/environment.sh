4b1aae0dcfc04897c2ac48378dd4057fd3bd0a36
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-09-08 21:41:11 +0800

    🏗 Migrate beeline to own repo

--- system/beeline/environment.sh
+++
@@ -1,5 +0,0 @@
-
-# Inside a node package
-#  @binaryben/dropbear
-
-# If not anywhere special, show weather and other nice to haves
--- system/beeline/system.sh
+++
@@ -1,4 +0,0 @@
-
-# Display OS icon
-
-# If over SSH, show username@hostname as well

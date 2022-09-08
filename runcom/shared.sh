fdff7fd31d25aed1cd8f483e66c290347a8ab302
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-09-09 00:00:23 +0800

    ⚡️ Fixed local env not overriding defaults

--- runcom/shared.sh
+++ runcom/shared.sh
@@ -8,6 +8,8 @@ fi
 
 reload() { # Helper to (re)load source files
 
+  # TODO: Should I implement a way to reload .zshrc and .bashrc files?
+
   if [[ -z $1 ]]; then # Load all dotfiles by default
 
     # Note: The order matters! Don't change it...
@@ -17,12 +19,22 @@ reload() { # Helper to (re)load source files
     reload path
     reload functions  # Simple fn that don't need their own script
     reload aliases
-    reload beeline    # Powerline inspired prompt segments
     reload prompt
   
   else # Load scope from ...
 
-    # ... $XDG_DOTFILES
+  # $HOME/.local first in case there are any local overrides ...
+
+    if [ -d $HOME/.local/system/$1 ]; then
+      for FILE in `find $HOME/.local/system/$1`
+      do
+        [ -f “$FILE” ] && source “$FILE”
+      done
+    fi
+
+    [ -f $HOME/.local/system/$1.sh ] && source $HOME/.local/system/$1.sh
+
+    # ... and then $XDG_DOTFILES
 
     if [ -d $XDG_DOTFILES/system/$1 ]; then
       for FILE in `find $XDG_DOTFILES/system/$1`
@@ -33,16 +45,6 @@ reload() { # Helper to (re)load source files
 
     [ -f $XDG_DOTFILES/system/$1.sh ] && source $XDG_DOTFILES/system/$1.sh
 
-    # ... and $HOME/.local
-
-    if [ -d $HOME/.local/system/$1 ]; then
-      for FILE in `find $HOME/.local/system/$1`
-      do
-        [ -f “$FILE” ] && source “$FILE”
-      done
-    fi
-
-    [ -f $HOME/.local/system/$1.sh ] && source $HOME/.local/system/$1.sh
   fi
 
 }; reload;

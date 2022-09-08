e7e5b348d172437ed761f096e7782b8e9f0046e9
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-09-09 00:12:11 +0800

    🧑‍💻 Tame the beast that is Node

---
+++ system/functions/dev.sh
@@ -0,0 +1,12 @@
+
+# Source: https://github.com/vmattos/kill-node
+# No licence available
+function kill-node {
+  NODE_PROCS=`ps -ax | grep node`
+  if [ $# -ne 0 ]; then
+    echo "Killing $1 whole family"
+    NODE_PROCS=`echo $NODE_PROCS | grep $1`
+  fi
+  echo "Killing every node processes. Not a single child will survive"
+  kill -15 `echo $NODE_PROCS | awk '{print $1}'` 2> /dev/null
+}

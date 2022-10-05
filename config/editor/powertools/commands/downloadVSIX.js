bf5ba00aa3f7f485948869c0079df6cfafb11a36
Benny Michaels <BinaryBen@users.noreply.github.com>
2022-10-05 19:42:34 +0800

    ✨ Extension downloader

--- config/editor/powertools/commands/downloadVSIX.js
+++ config/editor/powertools/commands/downloadVSIX.js
@@ -1,7 +1,137 @@
-exports.execute = async (args) => {
-  const vscode = args.require('vscode');
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 
-  vscode.window.showInformationMessage(
-    `Hello, from ${process.env.XDG_DOTFILES}`
-  );
-};
+const root = `${process.env.XDG_DOTFILES}/config/editor/profiles/core-full/data/extensions`
+
+const next = (unprocessedExtensions, processedExtensions, vscode, output) => {
+  if (unprocessedExtensions.length === 1) { // They're all done, this is just a remnant
+    if (processedExtensions.length === 0) {
+      vscode.window.showInformationMessage(`Cached extensions are already up to date`)
+      output.dispose()
+    } else {
+      output.clear()
+      output.appendLine(`Downloading ${processedExtensions.length} extension${processedExtensions.length == 1 ? '' : 's\n'}`)
+      downloadExtensions(processedExtensions, vscode, output)
+    }
+  } else if (unprocessedExtensions.length > 1) { // Still getting metadata
+    unprocessedExtensions.shift()
+    generateDownloadList(unprocessedExtensions, processedExtensions, vscode, output)
+  } else if (processedExtensions.length > 1) { // Still downloading
+    processedExtensions.shift()
+    downloadExtensions(processedExtensions, vscode, output)
+  } else { // And done!
+    vscode.window.showInformationMessage(`Extension update process finished`)
+    output.dispose()
+  }
+}
+
+const downloadExtensions = (extensions, vscode, output) => {
+  extension = extensions[0]
+
+  https.get(extension.url, (res) => {
+    let vsix = ''
+    res.setEncoding('binary')
+
+    if (res.statusCode === 200) {
+      output.appendLine(`Starting download of ${extension.name}`)
+      output.append('|=')
+
+      res.on('data', function(chunk){
+        output.append(`${new Date().getTime() % 5 === 0 ? '=' : ''}`)
+        vsix += chunk
+      })
+
+      res.on('end', function(){
+        output.append('|')
+        fs.writeFile(path.join(root, extension.filename), vsix, 'binary', function(err) {
+          if (err) {
+            output.appendLine(err)
+          } else {
+            output.append(` ✅ ${extension.filename} saved\n\n`)
+          }
+          setTimeout(() => {
+            next([], extensions, vscode, output)
+          }, 3500)
+        })
+      })
+    } else {
+      output.appendLine(`⛔️ Download URL for ${extension.name} returned ${res.statusCode} status code`)
+      output.appendLine(`   Manually download at: ${extension.url}\n`)
+      setTimeout(() => {
+        next([], extensions, vscode, output)
+      }, 8000) // Probably 429. Probably because hitting it too hard and fast.
+    }
+  })
+}
+
+const generateDownloadList = (unprocessedExtensions, processedExtensions, vscode, output) => {
+  const extension = unprocessedExtensions[0]
+  const publisher = extension.split('.')[0]
+  const slug = extension.split('.')[1]
+  const homepage = `https://marketplace.visualstudio.com/items?itemName=${extension}`
+
+  output.appendLine(`\nFetching homepage for ${extension}`)
+  output.append('Downloading metadata: |')
+
+  https.get(homepage, (res) => {
+    let body = ''
+
+    if (res.statusCode === 200) {
+      res.on('data', (data) => {
+        body += data.toString()
+        output.append('=')
+      })
+
+      res.on('end', () => {
+        let version = body.match( /\"version\":\"[0-9]*.[0-9]*.[0-9]*\"/ ).toString()
+        version = version.split(":")[1]
+        version = version.replace(/"/g,"")
+
+        let name = body.match( /<title>[\s\S]+<\/title>/ ).toString()
+        name = name.split("- Visual Studio Marketplace")[0]
+        name = name.replace(/<title>/g,"").replace(/\r?\n|\r/g, "").trim()
+
+        const filename = `${extension}-${version}.vsix`
+
+        output.append(`| ✅ ${name} (v${version})`)
+
+        downloadURL = `https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${publisher}/vsextensions/${slug}/${version}/vspackage`
+
+        if (!fs.existsSync(path.join(root, filename))) {
+          output.appendLine('')
+          processedExtensions.push({
+            name,
+            filename,
+            url: downloadURL,
+          })
+        } else {
+          output.append(' Status: Up to date\n')
+        }
+
+        next(unprocessedExtensions, processedExtensions, vscode, output)
+      })
+    } else {
+      output.append(` ⛔️ ${res.statusCode}\n`)
+      vscode.window.showErrorMessage(`Possible issue in manifest: ${extension} (${res.statusCode})`)
+      next(unprocessedExtensions, processedExtensions, vscode, output)
+    }
+  })
+}
+
+exports.execute = async (args) => {
+  const vscode = args.require('vscode')
+
+  const output = vscode.window.createOutputChannel('Powertools')
+  output.show()
+
+  fs.readFile(`${root}/manifest`, 'utf8', function(err, data) {
+    if (err) {
+      vscode.window.showErrorMessage(`Could not read the manifest at ${root}`)
+      output.dispose()
+    } else {
+      output.appendLine('Checking latest versions')
+      generateDownloadList(data.split(/\r?\n/), [], vscode, output)
+    }
+  })
+}
---
+++ config/editor/profiles/core-full/data/extensions/README.md
@@ -0,0 +1,3 @@
+These extensions are downloaded using a custom Power Tool command (Download extensions).
+
+The command extracts the version from the extensions homepage on the Visual Studio Marketplace to generate the download URL for the VSIX file.
